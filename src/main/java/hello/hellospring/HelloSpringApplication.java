8fd0b90c6b3e8e13ccf09e3b2e85976077aa5051
Hamin Jung <java020103@gmail.com>
2024-03-14 15:11:21 +0900

    LaptopConnectTest

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package hello.hellospring; 
+package hello.hellospring; //TestLaptopConnect
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
